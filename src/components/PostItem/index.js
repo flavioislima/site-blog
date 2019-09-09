2a434d718e9282c5319423ed7ce44c37b64f1886
Flavio F Lima <flavioislima@gmail.com>
2019-09-09 14:54:21 +0200

    Created PostItem Component

---
+++ src/components/PostItem/index.js
@@ -0,0 +1,39 @@
+import React from "react"
+import PropTypes from "prop-types"
+
+import * as S from "./styled"
+
+const PostItem = ({
+  slug,
+  background,
+  category,
+  date,
+  timeToRead,
+  title,
+  description,
+}) => {
+  return (
+    <S.PostItemLink to={slug}>
+      <S.PostItemWrapper>
+        <S.PostItemTag background={background}>{category}</S.PostItemTag>
+        <S.PostItemInfo>
+          <S.PostItemDate>{`${date} - ${timeToRead} minutes to read`}</S.PostItemDate>
+          <S.PostItemTitle>{title}</S.PostItemTitle>
+          <S.PostItemDescription>{description}</S.PostItemDescription>
+        </S.PostItemInfo>
+      </S.PostItemWrapper>
+    </S.PostItemLink>
+  )
+}
+
+PostItem.propTypes = {
+  slug: PropTypes.string.isRequired,
+  background: PropTypes.string,
+  title: PropTypes.string.isRequired,
+  category: PropTypes.string.isRequired,
+  date: PropTypes.string.isRequired,
+  timeToRead: PropTypes.string.isRequired,
+  description: PropTypes.string.isRequired,
+}
+
+export default PostItem
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,26 +1,21 @@
 import React from "react"
 
-import { Link } from "gatsby"
-
 import Layout from "../components/Layout"
 import SEO from "../components/Seo"
+import PostItem from "../components/PostItem"
 
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
-    <h1>Home</h1>
-    <ul>
-      <li>
-        <Link to="/" activeStyle={{ color: "red" }}>
-          Home
-        </Link>
-      </li>
-      <li>
-        <Link to="about/" activeStyle={{ color: "red" }}>
-          About
-        </Link>
-      </li>
-    </ul>
+    <PostItem
+      slug={"/about/"}
+      title={"Post Title"}
+      description={"Post Description"}
+      category={"Misc"}
+      background={"green"}
+      date={"3 days ago"}
+      timeToRead={5}
+    />
   </Layout>
 )
 
