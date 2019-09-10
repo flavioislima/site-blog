59ade025038f15934434c51a1e115d8815c06e3b
Flavio F Lima <flavioislima@gmail.com>
2019-09-10 10:58:52 +0200

    Added typescript to the project

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,6 +6,7 @@ module.exports = {
     author: `Flavio F Lima`,
   },
   plugins: [
+    `gatsby-plugin-typescript`,
     `gatsby-plugin-styled-components`,
     `gatsby-plugin-react-helmet`,
     {
